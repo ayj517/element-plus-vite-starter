6e7cfccb4cb838813e11c613cdaafb6e24aa5fb3
frxxzxjp <frxxzxjp@protonmail.com>
2022-05-08 19:13:28 +0800

    add login

---
+++ src/app.tsx
@@ -0,0 +1,47 @@
+import type { Settings as LayoutSettings } from '@ant-design/pro-layout';
+import { PageLoading } from '@ant-design/pro-layout';
+import type { RunTimeLayoutConfig } from 'umi';
+import UserInfo from '@/components/UserInfo'
+import { history } from 'umi';
+/** 获取用户信息比较慢的时候会展示一个 loading */
+export const initialStateConfig = {
+  loading: <PageLoading />,
+};
+
+/**
+ * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
+ * 登录校验在这里进行
+ * */
+export async function getInitialState(): Promise<{
+  settings?: Partial<LayoutSettings>;
+}> {
+
+
+  return {
+    settings: {}
+  };
+}
+
+// ProLayout 支持的api https://procomponents.ant.design/components/layout
+export const layout: RunTimeLayoutConfig = ({ initialState }) => {
+  const currentUser = initialState?.currentUser;
+
+
+  return {
+    // headerContentRender: () => <HeaderContainer />,
+    rightContentRender: () => <UserInfo/>,
+    disableContentMargin: false,
+    // footerRender: () => <Footer />,
+    onPageChange: () => {
+      // 如果没有登录，重定向到 login
+      // if (!initialState?.currentUser) login();
+
+      // // 根目录重定向到默认页面, 需要根据项目路由配置修改
+      // if (window.location.pathname === '/')
+      //   history.push(`/project/${currentProject?.appKey}/scene`);
+    },
+    headerRender: <div>adsfasdfasdf</div>,
+    logo: () => null,
+    ...initialState?.settings,
+  };
+};
--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,10 +1,101 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import styles from './index.less'
+
+import { Modal, Button, Form, message } from 'antd';
+
+import { LoginForm, ProFormText, ProFormCaptcha } from '@ant-design/pro-form';
+import { MobileOutlined, LockOutlined, } from '@ant-design/icons';
+import type { CSSProperties } from 'react';
 
 
 const UserInfo: FC = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-  return <>
-  </>
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return <div className={styles.userInfoContainer}>
+    <span className={styles.login} onClick={showModal} >
+      登录/注册
+    </span>
+
+    <Modal
+      className='login-modal'
+      title="手机验证码登录"
+      visible={isModalVisible}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      footer={null}
+    >
+      <LoginForm
+        className="login-form"
+        onFinish={
+          async (values) => {
+            console.log(values);
+            message.success('提交成功');
+          }
+        }
+      >
+        <>
+          <ProFormText
+            fieldProps={{
+              size: 'large',
+              prefix: <MobileOutlined className={'prefixIcon'} />,
+            }}
+            name="mobile"
+            placeholder={'手机号'}
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号！',
+              },
+              {
+                pattern: /^1\d{10}$/,
+                message: '手机号格式错误！',
+              },
+            ]}
+          />
+          <ProFormCaptcha
+            fieldProps={{
+              size: 'large',
+              prefix: <LockOutlined className={'prefixIcon'} />,
+            }}
+            captchaProps={{
+              size: 'large',
+            }}
+            placeholder={'请输入验证码'}
+            captchaTextRender={(timing: any, count: any) => {
+              if (timing) {
+                return `${count} ${'获取验证码'}`;
+              }
+              return '获取验证码';
+            }}
+            name="captcha"
+            rules={[
+              {
+                required: true,
+                message: '请输入验证码！',
+              },
+            ]}
+            onGetCaptcha={async () => {
+              message.success('获取验证码成功！验证码为：1234');
+            }}
+          />
+        </>
+      </LoginForm>
+
+
+    </Modal>
+  </div>
 }
 
 export default UserInfo
