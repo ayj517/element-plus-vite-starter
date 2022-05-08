0c27798871ea3abe67b557bae7f1ee0d670c0ea7
vico <vico@gmail.com>
2022-05-08 12:04:05 +0800

    fix:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import styles from './index.less';
+import GameLiveItem from '@/component/gameLive/GameLiveItem'
 
 export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <GameLiveItem/>
     </div>
   );
 }
